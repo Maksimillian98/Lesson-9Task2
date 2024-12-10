1f47d491b5ef79d760be27f28370f3811617fb6b
maxstar <maksimgavura78@gmail.com>
2024-12-10 22:09:08 +0200

    Добавьте файлы проекта.

---
+++ Lesson#9Task2/Lesson_9Task2.py
@@ -0,0 +1,5 @@
+﻿list1 = list(map(int, input("Введите первый список чисел через пробел: ").split()))
+list2 = list(map(int, input("Введите второй список чисел через пробел: ").split()))
+numbers = list1 + list2
+print("Количество общих чисел:", len(numbers))
+
